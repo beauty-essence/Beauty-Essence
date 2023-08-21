b4ad594a97aa86ed06eb499b1b91dcd3152c0766
Jakub Karaś <jakub.karas@onet.pl>
2023-08-21 18:55:08 +0200

    added tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      primary: ["Cormorant Garamond", "sans-serif"],
+      secondary: ["Jost", "sans-serif"],
+    },
+    colors: {
+      primary: "#F1CD84",
+      "primary-light": "#FFEBCF",
+      secondary: "#31392F",
+      "secondary-text": "#77846E",
+      "bg-white": "FEF8F5",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
