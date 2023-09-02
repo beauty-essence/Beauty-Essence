854a8a7bdb758bff9530e0a5b64e92cd012ad110
Igor Patro <59409890+IgorPatro@users.noreply.github.com>
2023-09-02 20:49:47 +0200

    Gallery
    * added HeroSection, added backgtound photo and main text, imported icons, todo: fix corretlness of fonts
    
    navigation not implemented because it is not fully designed yet
    
    * Basic file
    
    * Basic structure
    
    * Footer finished
    
    * Added contact section
    
    * Basic opinions
    
    * Temp
    
    * Testing react
    
    * framer-motion
    
    * created opinions section
    
    * Gallery added
    
    ---------
    
    Co-authored-by: Jakub Karaś <jakub.karas@onet.pl>
    Co-authored-by: Jakub Karaś <92886013+KarasJakub@users.noreply.github.com>

---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,95 @@
+import React from "react"
+import Masonry from "react-responsive-masonry"
+import one from "assets/gallery/one.png"
+import two from "assets/gallery/two.png"
+import three from "assets/gallery/three.png"
+import four from "assets/gallery/four.png"
+import five from "assets/gallery/five.png"
+import arrow_right from "assets/icons/arrow_right.svg"
+import arrow_left from "assets/icons/arrow_left.svg"
+import close from "assets/icons/close.svg"
+
+const images = [one, two, three, four, five]
+
+const Gallery = () => {
+  const [previewImageIndex, setPreviewImageIndex] = React.useState(0)
+  const [showPreview, setShowPreview] = React.useState(false)
+
+  const openPreview = (index: number) => {
+    setPreviewImageIndex(index)
+    setShowPreview(true)
+  }
+
+  const closePreview = () => {
+    setShowPreview(false)
+  }
+
+  const handleShowPrev = () => {
+    setPreviewImageIndex(
+      previewImageIndex === 0 ? images.length - 1 : previewImageIndex - 1
+    )
+  }
+
+  const handleShowNext = () => {
+    setPreviewImageIndex(
+      previewImageIndex === images.length - 1 ? 0 : previewImageIndex + 1
+    )
+  }
+
+  return (
+    <>
+      <section className="relative w-full flex justify-center items-center lg:py-16 bg-[#31392F]">
+        <div className="container md:flex md:gap-5 justify-center flex flex-col items-center p-10">
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-5xl font-primary font-bold text-white mb-2">
+              Galeria
+            </h1>
+            <p className="text-white max-w-sm text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut odio
+              sapien, congue a purus vitae, ultricies consequat nibh.
+            </p>
+          </div>
+          <Masonry columnsCount={3} gutter="10px" className="mt-10">
+            {images.map((image, index) => (
+              <img
+                src={image.src}
+                key={image.src}
+                className="cursor-pointer"
+                onClick={() => openPreview(index)}
+              />
+            ))}
+          </Masonry>
+          <a
+            href="/galeria"
+            className="mt-10 bg-primary px-10 py-4 hover:opacity-90 transition-opacity"
+          >
+            Zobacz więcej zdjęć
+          </a>
+        </div>
+      </section>
+      {showPreview && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+          <div className="bg-black w-full h-full fixed top-0 left-0 opacity-70"></div>
+          <img src={images[previewImageIndex].src} className="z-50" />
+          <img
+            src={close.src}
+            className=" w-12 h-12 absolute top-10 right-10 cursor-pointer"
+            onClick={closePreview}
+          />
+          <img
+            src={arrow_left.src}
+            className=" w-12 h-12 absolute bottom-5 right-1/2 md:top-1/2 md:left-5 md:right-auto cursor-pointer"
+            onClick={handleShowPrev}
+          />
+          <img
+            src={arrow_right.src}
+            className=" w-12 h-12 absolute bottom-5 left-1/2 md:top-1/2 md:right-5 md:left-auto cursor-pointer"
+            onClick={handleShowNext}
+          />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Gallery
