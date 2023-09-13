1498c7f5f92605eee1c769338e78778d619e8adc
Jakub Karaś <jakub.karas@onet.pl>
2023-09-13 22:38:49 +0200

    added properly displaying navigation based on paths

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,36 @@
 import HeroBackgroundImage from "assets/HeroBackgroundImage.png"
 import company_logo from "assets/company-logo-gold.svg"
 import NavigationButton from "./NavigationButton"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MobileNavigationList from "./MobileNavigationList"
 import { ROUTES } from "helpers/ROUTES"
 
 const Navigation = () => {
   const [toggled, setIsToggleed] = useState(false)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const handleNavigationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    window.addEventListener("popstate", handleNavigationChange)
+
+    return () => {
+      window.removeEventListener("popstate", handleNavigationChange)
+    }
+  }, [])
 
   return (
     <nav
       className="fixed w-full z-20 bg-top bg-cover"
-      // style={{ backgroundImage: `url(${HeroBackgroundImage.src})` }}
+      style={{
+        backgroundImage: `url(${
+          currentPath !== "/" ? HeroBackgroundImage.src : ""
+        })`
+      }}
     >
       {/* <img src={HeroBackgroundImage.src} alt="" className="w-full" /> */}
-      <div className="w-full hidden md:flex pt-12 justify-center gap-10 z-10">
+      <div className="w-full hidden md:flex py-8 justify-center gap-10 z-10">
         <ul className="flex gap-10 items-center">
           <a href={ROUTES.aboutUs}>
             <li className="font-detail text-white text-lg font-medium hover:scale-110 transition-transform">
