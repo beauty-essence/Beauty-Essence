0a3ab0ee755d517e4b947654e430db747aad26b0
Jakub Karaś <jakub.karas@onet.pl>
2025-05-16 00:13:36 +0200

    added popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,17 +1,18 @@
 import React from "react"
-import popup_image from "assets/popup_03_2025.png"
+import popup_image from "assets/popup_05_2025.jpg"
+
 import close_icon from "assets/icons/close_black.svg"
 import { useState } from "react"
 
 const Popup = () => {
-  const [isPopupOpened, setIsPopupOpened] = useState(false)
+  const [isPopupOpened, setIsPopupOpened] = useState(true)
   return (
     <div
       className={`w-full bg flex justify-center
       ${isPopupOpened ? "opacity-100" : "opacity-0"}
       `}
     >
-      <div className="w-[330px] h-[330px] md:w-[500px] md:h-[500px] fixed z-40 translate-y-[80%] md:translate-y-[3%]">
+      <div className="w-[330px] h-[330px] md:w-[500px] md:h-[500px] fixed z-40 translate-y-[80%] md:translate-y-[20%]">
         <img
           src={close_icon.src}
           alt=""
